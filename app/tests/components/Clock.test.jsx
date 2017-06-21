c91df1b40bfc37bbaeeef1b779fb00fa6c3fbec8
Daniel Platon <platon@adobe.com>
2017-06-21 23:10:17 +0300

    Add the Clock component and create tests for it

---
+++ app/tests/components/Clock.test.jsx
@@ -0,0 +1,37 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Clock = require('Clock');
+
+describe('Clock', () => {
+    it('should exist', () => {
+        expect(Clock).toExist();
+    });
+});
+
+describe('formatSeconds', () => {
+    it('should format seconds', () => {
+        var clock = TestUtils.renderIntoDocument(<Clock/>);
+        var seconds = 615;
+        var expected = '10:15';
+
+        var actualResult = clock.formatSeconds(seconds);
+
+        expect(actualResult).toBe(expected);
+
+    });
+
+    it('should format seconds with 0 padding', () => {
+        var clock = TestUtils.renderIntoDocument(<Clock/>);
+        var seconds = 61;
+        var expected = '01:01';
+
+        var actualResult = clock.formatSeconds(seconds);
+
+        expect(actualResult).toBe(expected);
+
+    });
+})
