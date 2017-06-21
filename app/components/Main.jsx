c879c902949d9d2a5ea507cf52ef70e870d9a2b1
Daniel Platon <platon@adobe.com>
2017-06-21 23:44:16 +0300

    Add the countdown form

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,11 +4,13 @@ var Nav = require('Nav');
 var Main = (props) => {
     return (
         <div>
-             <Nav/>
-                <div>
+            <Nav/>
+            <div className="row">
+                <div className="column small-centered medium-6 large-4">
                     {props.children}
                 </div>
             </div>
+        </div>
 
     )
 }
